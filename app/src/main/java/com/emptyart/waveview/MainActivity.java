2945ad3697fd4737a008d9335a52760209129edd
Rolando <rgarro@gmail.com>
2026-09-05 17:07:24 -0600

    es media noche en beiging ...

--- app/src/main/java/com/emptyart/waveview/MainActivity.java
+++ app/src/main/java/com/emptyart/waveview/MainActivity.java
@@ -3,7 +3,22 @@ package com.emptyart.waveview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
